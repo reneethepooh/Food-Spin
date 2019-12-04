f1be5ce8374f771a5cefdb0893a64ab8399a8d3c
Spyridon Coffinas <sgc332@nyu.edu>
2019-12-04 00:59:40 -0500

    finished page routing

--- food_spin/app/views.py
+++ food_spin/app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	return render(request, '../templates/intro.html')
+
+def spin(request):
+	return render(request, '../templates/spin.html')
+
+def result(request):
+	return render(request, '../templates/result.html')
+
+def signup(request):
+	return render(request, '../templates/signup.html')
+
+def pref(request):
+	return render(request, '../templates/pref.html')
--- food_spin/food_spin/urls.py
+++ food_spin/food_spin/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('spin', views.spin),
+    path('result', views.result),
+    path('signup', views.signup),
+    path('pref', views.pref)
 ]
