7d1888c4d19b82a92459bf716212b2fc1c642aaf
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-04 22:15:46 -0500

    Added new pref.html

---
+++ food_spin/app/EventForm.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class EventForm(forms.Form):
+    event_name = forms.CharField(label='eventname', max_length=100)
+    nickname = forms.CharField(label='eventname', max_length=100)
+    location=forms.CharField(label='eventname', max_length=100)
+    search_radius=forms.IntegerField(label='radius')
+    food_query=forms.CharField(label='food', max_length=100)
+    restrictions=forms.CharField(label='restrictions', max_length=100)
+    open_to_all=forms.BooleanField(label='opentoall',required=False)
+    gender_neutral=forms.BooleanFiel(label='gender_neutral',required=False)
+    weel_chair=forms.BooleanField(label='weel_chair',required=False)
+    waitlist=forms.BooleanField(label='waitlist',required=False)
+    cashback=forms.BooleanField(label='cashback',required=False)
+
+
--- food_spin/app/views.py
+++ food_spin/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect8
 
 def home(request):
 	return render(request, '../templates/intro.html')
@@ -12,5 +13,23 @@ def result(request):
 def signup(request):
 	return render(request, '../templates/signup.html')
 
+# def pref(request):
+# 	return render(request, '../templates/pref.html')
+
 def pref(request):
-	return render(request, '../templates/pref.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form=EventForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EventForm()
+
+    return render(request, '../templates/pref.html', {'form': form})
