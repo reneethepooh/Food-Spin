850db399b1d60701160118f2a2f88084a78fc34f
Spyridon Coffinas <sgc332@nyu.edu>
2019-12-05 23:25:31 -0500

    update models, user profile, etc.

--- food_spin/app/models.py
+++ food_spin/app/models.py
@@ -1,24 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
 class Restriction(models.Model):
 	name = models.CharField(max_length=20)
 
-class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE),
-	profile_picture = models.ImageField(upload_to='thumbpath', blank=True),
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(owner=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwards):
+	instance.profile.save()
+
+class Profile(models.Model):
+	owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True)
 	restrictions = models.ManyToManyField(Restriction)
 
 class Event(models.Model):
-	name = models.CharField(max_length=30),
-	leader = models.ForeignKey(User, on_delete=models.CASCADE),
-	followers = models.ManyToManyField(User),
-	link = models.TextField(),
-	status = models.CharField(max_length=15),
-	location = models.CharField(max_length=30),
-	radius = models.IntegerField()
+	name = models.TextField(default='My Event')
+	host = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hosting', null=True)
+	status = models.TextField(default='Started')
+	location = models.TextField(default='Manhattan')
+	radius = models.IntegerField(default=10)
+	link = models.TextField(null=True)
+	followers = models.ManyToManyField(User, related_name='participating')
 
 class EventSubmission(models.Model):
-	user = models.ForeignKey(UserProfile, on_delete=models.CASCADE),
-	event = models.ForeignKey(Event, on_delete=models.CASCADE),
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='submission')
+	event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='submissions')
 	preferences = models.ManyToManyField(Restriction)
