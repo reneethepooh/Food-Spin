bfee1d9d96aa13a798ed58e45fa5d8b4e41e71a5
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-05 20:54:54 -0500

    added Event Model Formq

--- food_spin/app/EventForm.py
+++ food_spin/app/EventForm.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Event
 
-class EventForm(ModelForm):
+class EventForm(forms.ModelForm):
         class Meta:
             model = Event
             exclude = ()
