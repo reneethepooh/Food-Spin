1626d357d025da54cd5a1a122f881c7f480111ca
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-08 13:16:13 -0500

    get event preference

--- food_spin/app/views.py
+++ food_spin/app/views.py
@@ -108,25 +108,15 @@ def profile_page(request):
 	return render(request, '../templates/profile.html', {'profile':profile,'form':form,'user':user})
 
 def result_page(request,slug):
-	# event = Event.objects.get(slug=slug)
-	# event_followers=event.followers.all()
-	# #get event submissions for all event followers:
-	# event_submission=[]
-	# for i in event_followers:
-	# 	event_submission.append(EventSubmission.objects.get(user=i,event=event))
+	event = Event.objects.get(slug=slug)
+	event_submissions=EventSubmission.objects.get(event=event)
+	event_preferences=[]
+	for submission in event_submissions.all:
+		for preference in submission.preference.all:
+			event_preferences.append(preference.name)
+	print(event_preferences)
 
-	# #also get the event submission from the host
-	# event_submission.append(EventSubmission.objects.get(user=event.host,event=event))
 
-	# #now that we have all event submissions, lets get the preferences of each one:
-	# event_food=[]
-	# for pref in event_submission:
-	# 	for restriction in event_submission[pref].preferences.all():
-	# 		event_food.append(restriction.name)
-	
-	# event_food=list(set(event_food)) #remove the duplicates using the set function in python
-
-	# restaurant_name=yelp_call(event.radius,event.location,event_food) #make the apicall, which returns restaurant name right now
 
 
 	# #display the result in the corresponding html page
