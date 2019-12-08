1e20c9b7671d674230d960a91d426463f8485f35
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-08 13:25:29 -0500

    get event preference

--- food_spin/app/views.py
+++ food_spin/app/views.py
@@ -112,9 +112,10 @@ def result_page(request,slug):
 	event_submissions=EventSubmission.objects.filter(event=event)
 	event_preferences=[]
 	for submission in event_submissions:
-		for preference in submission.preferences.all:
-			event_preferences.append(preference.name)
-	print(event_preferences)
+		print(submission.preferences.all)
+	# 	for preference in submission.preferences.all:
+	# 		event_preferences.append(preference.name)
+	# print(event_preferences)
 
 
 
