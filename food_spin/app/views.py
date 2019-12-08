bcdbd6eb34c0504ff7e2248acd4c7ab2a1f0dba5
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-08 15:02:23 -0500

    get event preference

--- food_spin/app/views.py
+++ food_spin/app/views.py
@@ -113,7 +113,7 @@ def result_page(request,slug):
 	print(submission)
 
 	for event in submission:
-		for preference in event.preferences.all:
+		for preference in event.preferences:
 			print(preference.name)
 	
 
